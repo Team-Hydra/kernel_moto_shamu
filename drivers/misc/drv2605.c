e9838d7a8c4d279fd0fc904a31dd43d82ee43b35
Matt Filetto <matt.filetto@gmail.com>
2015-04-26 14:54:02 -0700

    drv2605: add vibratrion strength sysfs node
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	drivers/misc/drv2605.c

--- drivers/misc/drv2605.c
+++ drivers/misc/drv2605.c
@@ -163,8 +163,7 @@
 #define I2C_RETRY_DELAY		20 /* ms */
 #define I2C_RETRIES		5
 
-static struct kobject *vibe_kobj;
-static int vibe_strength;
+static int rtp_strength = 0x7F;
 
 static struct drv260x {
 	struct class *class;
@@ -590,7 +589,7 @@ static void vibrator_enable(struct timed_output_dev *dev, int value)
 		if (mode != MODE_REAL_TIME_PLAYBACK) {
 			if (audio_haptics_enabled && mode == MODE_AUDIOHAPTIC)
 				setAudioHapticsEnabled(NO);
-			drv260x_set_rtp_val(vibe_strength);
+			drv260x_set_rtp_val(rtp_strength);
 			drv260x_change_mode(MODE_REAL_TIME_PLAYBACK);
 			vibrator_is_playing = YES;
 		}
@@ -939,14 +938,6 @@ static void probe_work(struct work_struct *work)
 	/* Read calibration results */
 	drv260x_read_reg_val(reinit_sequence, sizeof(reinit_sequence));
 
-	if (drv260x->use_default_calibration) {
-		reinit_sequence[3] = drv260x->default_calibration[0];
-		reinit_sequence[5] = drv260x->default_calibration[1];
-		reinit_sequence[7] = drv260x->default_calibration[2];
-		reinit_sequence[9] = drv260x->default_calibration[3];
-		reinit_sequence[11] = drv260x->default_calibration[4];
-	}
-
 	/* Read device ID */
 	device_id = (status & DEV_ID_MASK);
 	switch (device_id) {
@@ -1162,31 +1153,42 @@ static struct file_operations fops = {
 	.write = drv260x_write
 };
 
-static ssize_t pwmvalue_show(struct device *dev,
-                struct device_attribute *attr, char *buf)
+static ssize_t show_rtp_strength(struct device *dev,
+		struct device_attribute *attr, char *buf)
 {
-        size_t count = 0;
-        count += sprintf(buf, "%d\n", vibe_strength);
-        return count;
+	size_t count = 0;
+	count += sprintf(buf, "%d\n", rtp_strength);
+	return count;
 }
 
-static ssize_t pwmvalue_store(struct device *dev,
-                struct device_attribute *attr, const char *buf, size_t count)
+static ssize_t store_rtp_strength(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
 {
-	int vs = 0;
-        sscanf(buf, "%d ",&vs);
-        if (vs < 0 || vs > 127) vs = 100;
-	vibe_strength = vs;
-        return count;
+	int ret, input;
+	ret = sscanf(buf, "%d", &input);
+	if (ret != 1) {
+		return -EINVAL;
+	}
+
+	if (input > 100)
+		input = 100;
+	else if (input < 0)
+		input = 0;
+
+	rtp_strength = input + 27;
+	return count;
 }
 
-static DEVICE_ATTR(pwmvalue, (S_IWUSR|S_IRUGO), pwmvalue_show, pwmvalue_store);
+static DEVICE_ATTR(rtp_strength, (S_IWUSR|S_IRUGO),
+	show_rtp_strength, store_rtp_strength);
+
+struct kobject *drv2605_kobj;
+EXPORT_SYMBOL_GPL(drv2605_kobj);
 
 static int drv260x_init(void)
 {
 	int reval = -ENOMEM;
 
-	vibe_strength = REAL_TIME_PLAYBACK_STRENGTH;
 	drv260x = kmalloc(sizeof *drv260x, GFP_KERNEL);
 	if (!drv260x) {
 		printk(KERN_ALERT
@@ -1240,11 +1242,17 @@ static int drv260x_init(void)
 	wake_lock_init(&vibdata.wklock, WAKE_LOCK_SUSPEND, "vibrator");
 	mutex_init(&vibdata.lock);
 
-	printk(KERN_ALERT "drv260x: initialized\n");
+	drv2605_kobj = kobject_create_and_add("drv2605", NULL) ;
+	if (drv2605_kobj == NULL) {
+		printk(KERN_ALERT "drv260x: drv2605_kobj create_and_add failed\n");
+	}
 
-	vibe_kobj = kobject_create_and_add("vibrator", NULL);
-	if (!vibe_kobj) return 0;
-	reval = sysfs_create_file(vibe_kobj, &dev_attr_pwmvalue.attr);
+	reval = sysfs_create_file(drv2605_kobj, &dev_attr_rtp_strength.attr);
+	if (reval) {
+		printk(KERN_ALERT "drv260x: sysfs_create_file failed for rtp_strength\n");
+	}
+
+	printk(KERN_ALERT "drv260x: initialized\n");
 	return 0;
 
  fail6:
@@ -1262,7 +1270,7 @@ static int drv260x_init(void)
 
 static void drv260x_exit(void)
 {
-	kobject_del(vibe_kobj);
+	kobject_del(drv2605_kobj);
 	gpio_direction_output(drv260x->en_gpio, GPIO_LEVEL_LOW);
 	gpio_free(drv260x->en_gpio);
 	if (!IS_ERR(drv260x->vibrator_vdd))
