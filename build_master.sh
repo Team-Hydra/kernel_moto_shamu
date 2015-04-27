7281ce3569e31a597d5146fcfd22f9f856f5434d
Matt Filetto <matt.filetto@gmail.com>
2015-04-26 18:05:05 -0700

    switch to Linaro GCC 4.9 toolchain and see how things go

--- build_master.sh
+++ build_master.sh
@@ -21,8 +21,8 @@ export USE_CCACHE=1
 #Enable FIPS mode
 #export USE_SEC_FIPS_MODE=true
 export ARCH=arm
-export CROSS_COMPILE=/home/albinoman887/android/arm-eabi-4.8/bin/arm-eabi-
-#export CROSS_COMPILE=$PARENT_DIR/linaro4.9-a15/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=/home/albinoman887/android/arm-cortex_a15-linaro-4.9/bin/arm-eabi-
+#export CROSS_COMPILE=/home/albinoman887/android/arm-eabi-4.8/bin/arm-cortex_a15-linux-gnueabihf-
 #export CROSS_COMPILE=$SRC_ROOT/prebuilt/linux-x86/toolchain/linaro/bin/arm-eabi-
 #export CROSS_COMPILE=$SRC_ROOT/prebuilt/linux-x86/toolchain/new/bin/arm-cortex_a15-linux-gnueabihf-
 export ENABLE_GRAPHITE=true
